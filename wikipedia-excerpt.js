4b084a060c268e4be60e3b4555d6b756d6fc6731
Anna Henningsen <sqrt@entless.org>
2016-02-03 14:54:57 +0100

    Always take only first paragraph

--- wikipedia-excerpt.js
+++ wikipedia-excerpt.js
@@ -6,14 +6,14 @@ module.exports = function(body) {
 
   // Take the first paragraph containing a bold entry.
   // Best guess only, sorry.
-  const desc = $.root().children('p').filter(function(i, el) {
+  const desc = $($.root().children('p').filter(function(i, el) {
     return $(el).find('b').length > 0;
-  });
+  }).get(0));
   
   // If we and with a colon, this is probably some
   // kind of disambiguation or list
   if (/:$/.test(desc.text().trim())) {
-    return 0;
+    return null;
   }
   
   return desc.html();
